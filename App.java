599c1081f715c6b8ccdcacad4f6147d968aac052
Shawn Lim <shawnlim89@live.com>
2017-03-16 15:11:43 +0800

    Refactor and accept user input for commands

--- App.java
+++ App.java
@@ -1,15 +1,49 @@
 import blackjack.Blackjack;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
     Blackjack bj = new Blackjack();
 
-    bj.deal();
+    bj.start();
+    bj.sayTotal();
 
-    System.out.println(bj.total());
+    Scanner input = new Scanner(System.in);
 
-    bj.deal();
+    String command = "";
 
-    System.out.println(bj.total());
+    while(!command.equals("exit")) {
+      System.out.println("What do you want to do next? <deal|restart|total|exit>");
+      command = input.next();
+
+      switch (command) {
+        case "deal":
+          bj.deal();
+          bj.sayTotal();
+
+          if (bj.isBust()) {
+            System.out.println("You are busted!");
+          }
+          break;
+
+        case "restart":
+          bj.restart();
+          bj.sayTotal();
+          break;
+
+        case "total":
+          bj.sayTotal();
+          break;
+
+        case "exit":
+          break;
+
+        default:
+          System.out.println("You did not give a valid command!");
+          break;
+
+      }
+    }
+
   }
 }
--- blackjack/Blackjack.java
+++ blackjack/Blackjack.java
@@ -4,11 +4,27 @@ import java.util.ArrayList;
 
 public class Blackjack {
   private ArrayList<Integer> cards = new ArrayList<Integer>();
-  private int next = 0;
-  private int total = 0;
 
   public void Blackjack() {
 
+  }
+
+  public void dealFirstTwo() {
+    this.deal();
+    this.deal();
+  }
+
+  public void start() {
+    this.dealFirstTwo();
+
+    System.out.println("Game started");
+  }
+
+  public void restart() {
+    this.cards.clear();
+    this.dealFirstTwo();
+
+    System.out.println("Game restarted");
   }
 
   public void deal() {
@@ -16,16 +32,29 @@ public class Blackjack {
 
     cards.add(card);
 
-    next++;
+    if (this.total() == 21) {
+      System.out.println("Blackjack!");
+    }
+    else if (cards.size() >= 5) {
+      System.out.println("5-Dragon!");
+    }
   }
 
   public int total() {
-    total = 0;
+    int total = 0;
 
     for (int i = 0; i < cards.size(); i++) {
       total+= cards.get(i);
     }
 
     return total;
+  }
+
+  public boolean isBust() {
+    return this.total() > 21;
+  }
+
+  public void sayTotal() {
+    System.out.println("Your total is: " + Integer.toString(this.total()));
   }
 }
